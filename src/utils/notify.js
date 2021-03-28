86eccfd7ace9351580fcc8daf4f392b86364c325
Émile Perron <contact@emileperron.com>
2021-03-28 14:55:58 -0400

    Removed axios dependency

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,14 +1,35 @@
-const axios = require("axios");
-const webhookUrl = process.env.WEBHOOK_URL || null;
+const querystring = require("querystring");
+const https = require("https");
+const webhookHost = process.env.WEBHOOK_HOST || null;
+const webhookPath = process.env.WEBHOOK_PATH || null;
 
 module.exports = class Notify {
 	static _post(body) {
-		if (webhookUrl) {
-			return axios.post(webhookUrl, body).catch((error) => {
-				console.error(error);
+		if (webhookHost) {
+			const postData = querystring.stringify(body);
+			const options = {
+				hostname: webhookHost,
+				port: 443,
+				path: webhookPath,
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+					"Content-Length": postData.length
+				},
+				rejectUnauthorized: false,
+				requestCert: true,
+				agent: false
+			};
+			const req = https.request(options);
+
+			req.on("error", (e) => {
+				console.error(e);
 			});
+
+			req.write(postData);
+			req.end();
 		} else {
-			throw new Error("You must define the WEBHOOK_URL environment variable.");
+			throw new Error("You must define the WEBHOOK_HOST and WEBHOOK_PATH environment variable.");
 		}
 	}
 
