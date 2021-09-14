8d4020640be5c7877dcfc3478cbf938c7ce992dd
Émile Perron <contact@emileperron.com>
2021-09-14 08:40:22 -0400

    Removes querystring dependency (replaced by URLSearchParams) and adds unit test for Notify

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,31 +1,42 @@
-const querystring = require("querystring");
 const http = require("http");
 const webhookHost = process.env.WEBHOOK_HOST || null;
 const webhookPath = process.env.WEBHOOK_PATH || null;
 
 module.exports = class Notify {
+	static _stringifyBody(body)
+	{
+		const postParams = new URLSearchParams({
+			payload: JSON.stringify(body)
+		});
+
+		return postParams.toString();
+	}
+
+	static _prepareOptions(queryString)
+	{
+		return {
+			hostname: webhookHost,
+			path: webhookPath,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": queryString.length
+			},
+			agent: false
+		};
+	}
+
 	static _post(body) {
 		if (webhookHost) {
-			const postData = querystring.stringify({
-				payload: JSON.stringify(body)
-			});
-			const options = {
-				hostname: webhookHost,
-				path: webhookPath,
-				method: "POST",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-					"Content-Length": postData.length
-				},
-				agent: false
-			};
+			const postQueryString = this._stringifyBody(body);
+			const options = this._prepareOptions(postQueryString);
 			const req = http.request(options);
 
 			req.on("error", (e) => {
 				console.error(e);
 			});
 
-			req.write(postData);
+			req.write(postQueryString);
 			req.end();
 		} else {
 			throw new Error("You must define the WEBHOOK_HOST and WEBHOOK_PATH environment variable.");
---
+++ test/utils/notifyTest.js
@@ -0,0 +1,45 @@
+const Notify = require("../../src/utils/notify.js");
+const assert = require("assert");
+const webhookPath = process.env.WEBHOOK_PATH || null;
+const webhookHost = process.env.WEBHOOK_HOST || null;
+
+describe("Notify (src/utils/notify.js)", () => {
+	it("Should build the query string from a JSON object", () => {
+		const body = {
+			request: {
+				id: 281,
+				url: "https://www.toituresbellevue.com/",
+				priority: 1,
+				tool: "@koalati/tool-seo",
+				received_at: "2021-03-29T01:14:12.125Z",
+				processed_at: null,
+				completed_at: null,
+				processing_time: null
+			},
+			results: [
+			],
+			processingTime: 1000,
+			success: true,
+			type: "toolSuccess"
+		};
+		const expectedQueryString = "payload=%7B%22request%22%3A%7B%22id%22%3A281%2C%22url%22%3A%22https%3A%2F%2Fwww.toituresbellevue.com%2F%22%2C%22priority%22%3A1%2C%22tool%22%3A%22%40koalati%2Ftool-seo%22%2C%22received_at%22%3A%222021-03-29T01%3A14%3A12.125Z%22%2C%22processed_at%22%3Anull%2C%22completed_at%22%3Anull%2C%22processing_time%22%3Anull%7D%2C%22results%22%3A%5B%5D%2C%22processingTime%22%3A1000%2C%22success%22%3Atrue%2C%22type%22%3A%22toolSuccess%22%7D";
+
+		assert.strictEqual(Notify._stringifyBody(body), expectedQueryString);
+	});
+
+	it("Should build POST request options", () => {
+		const queryString = Notify._stringifyBody({ test: "value" });
+		const expectedOptions = {
+			hostname: webhookHost,
+			path: webhookPath,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": queryString.length
+			},
+			agent: false
+		};
+
+		assert.deepEqual(Notify._prepareOptions(queryString), expectedOptions);
+	});
+});
