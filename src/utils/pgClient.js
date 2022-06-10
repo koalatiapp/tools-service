9635cce2d68616aaae52a18989f6799a73a2ce46
Émile Perron <contact@emileperron.com>
2022-06-10 11:16:05 -0400

    feat: add logs for pg events

--- src/utils/pgClient.js
+++ src/utils/pgClient.js
@@ -1,10 +1,29 @@
 const { Client } = require("pg");
 const config = require("../config/pg.js");
+let clientCount = 1;
+let openCount = 0;
 
 module.exports = () => {
+	const clientId = clientCount;
+	clientCount += 1;
+
 	const client = new Client(config);
 
-	client.connect();
+	client.connect(err => {
+		if (err) {
+			console.error(`postgres connection error (${clientId})`, err.stack);
+		} else {
+			openCount++;
+			console.log(`established postgres connection ${clientId} (${openCount} open connections)`);
+		}
+	});
+
+	client.on("error", err => console.error("postgres error:", err.stack));
+	client.on("notice", msg => console.warn("postgres notice:", msg));
+	client.on("end", () => {
+		openCount--;
+		console.log(`closed postgres connection ${clientId} (${openCount} open connections)`);
+	});
 
 	return client;
 };
