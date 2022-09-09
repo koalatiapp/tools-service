5fd32e73d80f7edb51ebd5cc149494d615231189
Émile Perron <contact@emileperron.com>
2022-09-09 15:41:02 -0400

    refactor: make database unit test ignorant of schema

--- test/utils/databaseTest.js
+++ test/utils/databaseTest.js
@@ -4,7 +4,7 @@ const assert = require("assert");
 describe("Database (src/utils/database.js)", () => {
 	it("Can connect to the database", async () => {
 		const database = await createDatabaseConnection();
-		const [rows] = await database.query("SELECT COUNT(*) FROM requests");
+		const [rows] = await database.query("SELECT 1");
 
 		assert.equal(rows.length, 1);
 
